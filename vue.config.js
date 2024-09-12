f180eab8bf710fe14064f2073b3c1fbd0be74171
FumaKado <maytheforcebewithyou2002@gmail.com>
2024-09-12 20:03:32 +0900

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  outputDir: 'docs',
-  assetsDir: './',
-  publicPath: './'
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue_practice_bus_guide/' : '/',
+  outputDir: 'docs'
 })
