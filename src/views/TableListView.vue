3f87e90735d0fe50a68778b281f0edcb1eea76ef
FumaKado <maytheforcebewithyou2002@gmail.com>
2024-09-12 17:10:41 +0900

    install view

---
+++ src/views/TableListView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { inject } from "vue"
+
+import AllHeader from "@/components/parts/AllHeader.vue"
+import TableList from "@/components/parts/TableList.vue"
+import Loading from "@/components/parts/Loading.vue"
+
+const busInfo = inject("busInfo")
+</script>
+
+<template>
+  <AllHeader/>
+  <v-main>
+    <TableList v-if="busInfo" :bus-info="busInfo"/>
+    <Loading v-else/>
+  </v-main>
+</template>
+
+<style scoped></style>
---
+++ src/views/TableView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { inject, computed } from "vue"
+
+import AllHeader from "@/components/parts/AllHeader.vue"
+import RouteInfo from "@/components/parts/RouteInfo.vue"
+import TimeTable from "@/components/templates/TimeTable.vue"
+import Loading from "@/components/parts/Loading.vue"
+
+const props = defineProps(["timeTableId"])
+const busInfo = inject("busInfo")
+
+const busRouteInfo = computed(() => {
+  if (busInfo.value) {
+    return busInfo.value["時刻表情報"].filter((item) => item.time_table_id === Number(props.timeTableId))[0]
+  }
+})
+</script>
+
+<template>
+  <AllHeader/>
+  <v-main>
+    <RouteInfo v-if="busInfo" :bus-route-info="busRouteInfo"></RouteInfo>
+    <TimeTable v-if="busInfo" :bus-info="busInfo" :time-table-id="timeTableId"/>
+    <Loading v-else/>
+  </v-main>
+</template>
+<style scoped></style>
