de2290e8c0bb8dee7d073d3893932464622deee3
FumaKado <maytheforcebewithyou2002@gmail.com>
2024-09-12 17:15:03 +0900

    update router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TableView from '@/views/TableView.vue'
+import TableListView from '@/views/TableListView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    props: true
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/tables',
+    name: 'tables',
+    component: TableListView,
+  },
+  {
+    path: '/tables/:timeTableId',
+    name: 'tablesDetail',
+    component: TableView,
+    props: true
   }
 ]
 
