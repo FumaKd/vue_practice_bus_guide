77b582ab01f5e1778af9f05660d154987747e44c
FumaKado <maytheforcebewithyou2002@gmail.com>
2024-09-12 17:48:34 +0900

    remove comment out

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,6 @@ provide("timeInfo", {hours, minutes, seconds, years, month, date, day})
 </script>
 
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/> -->
   <v-app>
     <RouterView/>
   </v-app>
